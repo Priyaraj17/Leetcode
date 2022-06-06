1375ac9e7299e46f6782bec72c2345c80bbd4820
Priyaraj <priyaraj.17@outlook.com>
2022-06-06 23:49:50 +0530

    Time: 39 ms (32.93%), Space: 8.2 MB (79.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,21 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int start = 0, end = 0, maxLen = 0;
-        unordered_map<char, int> freq;
-        for(end = 0; end < s.size(); end++) {
-            char ch = s[end];
-            if(freq.find(ch) != freq.end()) {
-                start = max(start, freq[ch] + 1);
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
+        {
+            int start = 0, maxLen = 0;
+            unordered_map<char, int> freq;
+            for (int e = 0; e < s.size(); e++)
+            {
+                char ch = s[e];
+                freq[ch]++;
+                while (start < e and freq[ch] > 1)
+                {
+                    freq[s[start]]--;
+                    start++;
+                }
+                maxLen = max(maxLen, e - start + 1);
             }
-            freq[ch] = end;
-            maxLen = max(maxLen, end - start + 1);
+            return maxLen;
         }
-        return maxLen;
-    }
 };
